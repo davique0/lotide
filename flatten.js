bf6e1fe2938e99c7f4b62393ba0f429e4b41920c
davique0 <davique@gmail.com>
2022-10-22 15:44:59 -0400

    flatten function added checking if there is an array nested

--- flatten.js
+++ flatten.js
@@ -18,3 +18,17 @@ const assertArrayEqual = function(arr1, arr2) {
     console.log(`❌❌❌ Assertion Failed: [${arr1}] is different than [${arr2}]`);
   }
 };
+
+const flatten = function(nestedArr) {
+  //console.log(nestedArr);
+  //checking every element of the array and find if there is an array nested
+  for(let i = 0; i < nestedArr.length; i++) {
+    const num = nestedArr[i];
+    if (Array.isArray(num) === true) {
+      // console.log(item);
+      //once we find the nested array take the items out of that array
+    }
+  }
+}
+
+flatten([1, 2, 3, [4, 5], 6]);
