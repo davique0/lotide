cf0d205713c1472a78f7823ef472be05c428c6cd
davique0 <davique@gmail.com>
2022-10-24 20:15:21 -0400

    counting just the items in itemsToCount

--- countOnly.js
+++ countOnly.js
@@ -10,15 +10,18 @@ const assertEqual = (actual, expected) => {
 const countOnly = function(allItems, itemsToCount) {
   let results = {};
   for (let item of allItems) {
-    if (results[item]) {
-      results[item] += 1;
-    } else {
-      results[item] = 1;
+    if(itemsToCount[item]) {
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+      // console.log(results);
     }
-    console.log(results);
   }
+  console.log(results);
   return results;
-}
+};
 
 const firstNames = [
   "Karl",
