8c5131bd50b9805396286247ac29c4d09d45223b
davique0 <davique@gmail.com>
2022-10-24 20:21:16 -0400

    creation of countLetter.js file with assertEqual function and countLetters function declaration

---
+++ countLetters.js
@@ -0,0 +1,11 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(str) {
+  let result = {};
+}
