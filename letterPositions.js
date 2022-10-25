1d42f66bf821908be913a531474271287f12919a
davique0 <davique@gmail.com>
2022-10-25 08:35:19 -0400

    file creation with AssertEqual and AssertArrayEqual

---
+++ letterPositions.js
@@ -0,0 +1,24 @@
+//Assert Equal to compare values
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+//Assert equal for array
+const assertArrayEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] is equal to [${arr2}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${arr1}] is different than [${arr2}]`);
+  }
+};
+
+
+const letterPositions = function(sentence) {
+let result = {};
+
+return result;
+}
