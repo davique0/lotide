faccab35ad019f23df24aa138dfb0cab1e4c4be3
davique0 <davique@gmail.com>
2022-10-24 20:30:17 -0400

    counter added, also string without spaces

--- countLetters.js
+++ countLetters.js
@@ -8,4 +8,17 @@ const assertEqual = (actual, expected) => {
 
 const countLetters = function(str) {
   let result = {};
+  let strNoSpace = str.split(" ").join("");
+  console.log(strNoSpace);
+  for (let item of strNoSpace) {
+    // console.log(item)
+    if (result[item]) {
+      result[item] += 1;
+    } else {
+      result[item] = 1;
+    }
+  }
+  return result;
 }
+
+console.log(countLetters("lighthouse in the house"));
