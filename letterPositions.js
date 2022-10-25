e5c07031eefb75f54f3757be5821be5b48a70074
davique0 <davique@gmail.com>
2022-10-25 09:34:00 -0400

    insterting letter as key in an empty object

--- letterPositions.js
+++ letterPositions.js
@@ -19,6 +19,24 @@ const assertArrayEqual = function(arr1, arr2) {
 
 const letterPositions = function(sentence) {
 let result = {};
+let sentLetter = '';
+let sentNoSpace = sentence.split(" ").join("");
 
+for (let item of sentNoSpace) {
+  result[item] = item;
+}
+
+// for (let i = 0; i < sentNoSpace.length; i++) {
+//   sentIndex = sentNoSpace[i]
+//   if (result[sentLetter]) {
+//     // console.log(sentLetter)
+//     result[sentLetter].push(i);
+//   } else {
+//     console.log(sentLetter)
+//     result[sentLetter] = [i];
+//   }
+// }
 return result;
 }
+
+console.log(letterPositions('hola'));
