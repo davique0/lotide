0606a87d67875189227ca775a0d8d6b86e808865
davique0 <davique@gmail.com>
2022-10-26 18:54:40 -0400

    function working with primitive type values

--- eqObjects.js
+++ eqObjects.js
@@ -7,7 +7,22 @@ const assertEqual = (actual, expected) => {
 };
 
 const eqObjects = function(object1, object2) {
-
+  let objOneKeyArr = Object.keys(object1);
+  let objTwoKeyArr = Object.keys(object2);
+  if(objOneKeyArr.length === objTwoKeyArr.length) {
+    for (let i = 0; i < objOneKeyArr.length; i++) {
+      if (!objOneKeyArr.includes(objTwoKeyArr[i])) {
+        return false;
+      } else {
+          for (const key in object1) {
+            if (object1[key] !== object2[key]) {
+              return false;
+            }
+        }
+      }
+    }
+  }
+  return true;
 };
 
 const ab = {
@@ -17,9 +32,9 @@ const ab = {
 
 const ba = {
   b: '2',
-  a: '1'
+  a: '3'
 };
 
-eqObjects(ab, ba); 
+console.log(eqObjects(ab, ba)); 
 
-eqObjects(ab, bas);
+
