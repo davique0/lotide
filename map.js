95ce02574058f7fac7920958989997f820877f28
davique0 <davique@gmail.com>
2022-10-28 12:41:50 -0400

    test code added

--- map.js
+++ map.js
@@ -30,4 +30,8 @@ const map = function(array, callback) {
 };
 
 const results1 = map(words, word => word[0]);
-console.log(results1);
+assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
+
+const arrTestNums = [23, 09, 98, 87, 4, 8, 89, 34, 78, 39, 6, 9 ]
+
+const evenOrOdd = map(arrTestNums, num => num % 2 === 0 ? console.log(`${num} is even`) : console.log(`${num} is odd`));
