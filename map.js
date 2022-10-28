5d862095add3c4682b9c795cf4adcf21f4803a03
davique0 <davique@gmail.com>
2022-10-28 11:48:38 -0400

    assertArraysequal and eqArrays copied in the file

--- map.js
+++ map.js
@@ -1,3 +1,24 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: [${arr1}] is equal to [${arr2}]`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: [${arr1}] is different than [${arr2}]`);
+  }
+};
+
 const words = ["ground", "control", "to", "major", "tom"];
 
 const map = function(array, callback) {
