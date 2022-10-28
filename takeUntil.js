26d939954df540cf0ae76af51092de27c18ddfac
davique0 <davique@gmail.com>
2022-10-28 18:18:36 -0400

    function working

--- takeUntil.js
+++ takeUntil.js
@@ -1,7 +1,7 @@
 const takeUntil = (array, callback) => {
  let results = [];
  for (const x of array) {
-  if (!callback) {
+  if (!callback(x)) {
     results.push(x)
   } else {
     return results;
