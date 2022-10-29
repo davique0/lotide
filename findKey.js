384b42cf68f02eb441406bd67151a6709b81b047
davique0 <davique@gmail.com>
2022-10-29 12:52:24 -0400

    find key function working

--- findKey.js
+++ findKey.js
@@ -6,5 +6,23 @@ const assertEqual = (actual, expected) => {
   }
 };
 
-assertEqual('Lighthouse Labs', 'Bootcamp');
-assertEqual(1, 1);
+const findKey = (obj, callback) => {
+  for (const key in obj) {
+    if (callback(obj[key])) {
+      return  key;
+    }
+  }
+}
+
+
+
+
+
+console.log(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2));
