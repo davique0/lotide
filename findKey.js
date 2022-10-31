e9c1d024c66aeb1e64df0194c8f51e23c213aad5
davique0 <davique@gmail.com>
2022-10-31 10:25:56 -0400

    findKey with test code assertEqual

--- findKey.js
+++ findKey.js
@@ -18,11 +18,13 @@ const findKey = (obj, callback) => {
 
 
 
-console.log(findKey({
+let testCode = {
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
   "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2));
+};
+
+assertEqual(findKey(testCode, x => x.stars === 3), 'Akaleri');
