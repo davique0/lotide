484406108a10fd16e9bde4c5437600dc646cf4f2
davique0 <davique@gmail.com>
2022-11-12 11:09:28 -0500

    assertObjectEqual refactored, exported and test code removed

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,34 +1,24 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
 
-const eqObjects = function(object1, object2) {
-  let objOneKeyArr = Object.keys(object1);
-  let objTwoKeyArr = Object.keys(object2);
-  if (objOneKeyArr.length !== objTwoKeyArr.length) {
-    return false;
-  } else {
-    for (const key of objOneKeyArr) {
-      if (Array.isArray(object1[key])) {
-        if (!eqArrays(object1[key], object2[key])) {
-          return false;
-        }
-      } else if (object1[key] !== object2[key]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
+// const eqObjects = function(object1, object2) {
+//   let objOneKeyArr = Object.keys(object1);
+//   let objTwoKeyArr = Object.keys(object2);
+//   if (objOneKeyArr.length !== objTwoKeyArr.length) {
+//     return false;
+//   } else {
+//     for (const key of objOneKeyArr) {
+//       if (Array.isArray(object1[key])) {
+//         if (!eqArrays(object1[key], object2[key])) {
+//           return false;
+//         }
+//       } else if (object1[key] !== object2[key]) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// };
 
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -39,6 +29,9 @@ const assertObjectsEqual = function(actual, expected) {
   }
   // console.log(`Example label: ${inspect(actual)}`);
 }
+
+module.exports = assertObjectsEqual;
+
 let objTest = {a: 1, b: 2};
 let objTest2 = {a: 1, b: 2};
 
