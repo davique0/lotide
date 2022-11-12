5132c4909c8af0c39c4419413cdeadfd725f2f7f
davique0 <davique@gmail.com>
2022-11-12 11:25:04 -0500

    countLetters refactored, exported and working

--- countLetters.js
+++ countLetters.js
@@ -1,26 +1,13 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const countLetters = function(str) {
   let result = {};
   let strNoSpace = str.split(" ").join("");
+  
   for (let item of strNoSpace) {
-    if (result[item]) {
-      result[item] += 1;
-    } else {
-      result[item] = 1;
-    }
+    result[item] ? result[item] += 1 : result[item] = 1;
   }
   return result;
 };
 
-const result1 = countLetters("lighthouse in the house");
-assertEqual(result1['h'], 4);
-assertEqual(result1['l'], 1);
-assertEqual(result1['u'], 2);
-assertEqual(result1[' '], undefined);
+module.exports = countLetters;
+
+
