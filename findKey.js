8a09941d050fc850cfed19c985eaa62615fa1d34
davique0 <davique@gmail.com>
2022-11-12 11:46:18 -0500

    findKeyTest created and working, findKey refactored, exported and test code removed

--- findKey.js
+++ findKey.js
@@ -1,10 +1,4 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//finds the key of the return of the callback function
 
 const findKey = (obj, callback) => {
   for (const key in obj) {
@@ -14,17 +8,4 @@ const findKey = (obj, callback) => {
   }
 }
 
-
-
-
-
-let testCode = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-};
-
-assertEqual(findKey(testCode, x => x.stars === 3), 'Akaleri');
+module.exports = findKey;
