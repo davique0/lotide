d17c92d99912187307d053b085e2cbf2eaae3c6d
davique0 <davique@gmail.com>
2022-11-12 11:50:59 -0500

    findKeyByValueTest created and working, findKeyByValue file refactored, exported and test code removed

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,10 +1,4 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//function finds the key of the value entered as second argument
 
 const findKeyByValue = function(obj, value) {
   let result = undefined;
@@ -16,11 +10,4 @@ const findKeyByValue = function(obj, value) {
   return result;
 };
 
-const bestTVShowsByGenre = {
-  sci_fi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+module.exports = findKeyByValue;
