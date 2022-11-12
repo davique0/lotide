909eed9fecf9e337aca1f60268c438eb5a6ad83c
davique0 <davique@gmail.com>
2022-11-11 23:32:58 -0500

    assertArraysEqual refactored, exported adn test code removed

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,6 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
+const eqArrays = require('./eqArrays');
 
-const assertArraysEqual = function(arr1, arr2) {
+const assertArraysEqual = (arr1, arr2) => {
   if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: [${arr1}] is equal to [${arr2}]`);
   } else {
@@ -19,5 +8,5 @@ const assertArraysEqual = function(arr1, arr2) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual(['hi', 'hello', 'hola'], ['bye', 'goodbye']);
+module.exports = assertArraysEqual;
+
