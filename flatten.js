7430cab46dc112f87ecdf9cc6799ed489f40af35
davique0 <davique@gmail.com>
2022-11-12 12:01:04 -0500

    flattenTest created and working, flatten refactored, exported and test code removed

--- flatten.js
+++ flatten.js
@@ -1,28 +1,9 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < arr1.length; i++) {
-      if (arr1[i] !== arr2[i]) {
-        return false;
-      }
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅✅✅ Assertion Passed: [${arr1}] is equal to [${arr2}]`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: [${arr1}] is different than [${arr2}]`);
-  }
-};
+//functions flattens an array inside an array up to 1 layer
 
 const flatten = function(nestedArr) {
   let flattenedArr = [];
   for(let i = 0; i < nestedArr.length; i++) {
-   if (Array.isArray(nestedArr[i]) !== true) {
+   if (!Array.isArray(nestedArr[i])) {
     flattenedArr.push(nestedArr[i]);
    } else {
     for (let j = 0; j < nestedArr[i].length; j++) {
@@ -33,4 +14,6 @@ const flatten = function(nestedArr) {
   return flattenedArr;
 }
 
-flatten([1, 2, 3, [4, 5], 6]);
+module.exports = flatten;
+
+
