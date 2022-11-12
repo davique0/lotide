1bb2eb389ce78489aaf02a6e4991208ee530d1f3
davique0 <davique@gmail.com>
2022-11-11 23:16:46 -0500

    refactor with module exports, test code removed

--- tail.js
+++ tail.js
@@ -1,14 +1,6 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//const assertEqual = require('./assertEqual');
 
+//removes first item of an array
 const tail = userArr => userArr.slice(1);
 
-const words = ["Hello", "Hi", "Hola"];
-const result = tail(words);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Hi");
+module.exports = tail;
