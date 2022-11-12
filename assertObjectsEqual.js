de9036e10bc5dbf0964829c6d49f8c359a2bccc0
davique0 <davique@gmail.com>
2022-11-12 11:26:39 -0500

    cleaned some whitespaces

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,25 +1,6 @@
 const eqArrays = require('./eqArrays');
 const eqObjects = require('./eqObjects');
 
-// const eqObjects = function(object1, object2) {
-//   let objOneKeyArr = Object.keys(object1);
-//   let objTwoKeyArr = Object.keys(object2);
-//   if (objOneKeyArr.length !== objTwoKeyArr.length) {
-//     return false;
-//   } else {
-//     for (const key of objOneKeyArr) {
-//       if (Array.isArray(object1[key])) {
-//         if (!eqArrays(object1[key], object2[key])) {
-//           return false;
-//         }
-//       } else if (object1[key] !== object2[key]) {
-//         return false;
-//       }
-//     }
-//   }
-//   return true;
-// };
-
 const assertObjectsEqual = function(actual, expected) {
   const inspect = require('util').inspect;
   if (eqObjects(actual, expected)) {
@@ -27,12 +8,6 @@ const assertObjectsEqual = function(actual, expected) {
   } else {
     console.log(`❌❌❌ Assertion Failed: ${inspect(actual)} is different than ${inspect(expected)}`);
   }
-  // console.log(`Example label: ${inspect(actual)}`);
 }
 
 module.exports = assertObjectsEqual;
-
-let objTest = {a: 1, b: 2};
-let objTest2 = {a: 1, b: 2};
-
-assertObjectsEqual(objTest, objTest2);
