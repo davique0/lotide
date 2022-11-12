a92ab21d349dd9b1808a6e8a4c34714b0098dcf2
davique0 <davique@gmail.com>
2022-11-12 10:20:36 -0500

    middleTest converted for Mocha ready

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,20 @@
-//const eqArrays = require('./eqArrays');
-const assertArraysEqual = require('../assertArraysEqual');
+// const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-const  testArr = [1, 2, 3, 4, 5, 6, 7, 8];
-assertArraysEqual(middle(testArr), [4, 5]);
+describe("#middle", () => {
+  it("returns [4, 5] for [1, 2, 3, 4, 5, 6, 7, 8]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6, 7, 8]), [4, 5]);
+  });
 
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+
+});
+
+
+// const  testArr = [1, 2, 3, 4, 5, 6, 7, 8];
+// assertArraysEqual(middle(testArr), [4, 5]);
+
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
