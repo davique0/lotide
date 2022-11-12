d443675af10465c2136d8ae8f872ad74cd3caae4
davique0 <davique@gmail.com>
2022-11-12 10:21:13 -0500

    middleTest cleaned

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,3 @@
-// const assertArraysEqual = require('../assertArraysEqual');
 const assert = require('chai').assert;
 const middle = require('../middle');
 
@@ -12,9 +11,3 @@ describe("#middle", () => {
   });
 
 });
-
-
-// const  testArr = [1, 2, 3, 4, 5, 6, 7, 8];
-// assertArraysEqual(middle(testArr), [4, 5]);
-
-// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
