ba89568296689e9df98a039b26343b54abb8a64c
davique0 <davique@gmail.com>
2022-11-12 12:27:37 -0500

    index file importing all fuinctions in lotide and exporting them in an object

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ const middle = require('./middle');
 const tail = require('./tail');
 const assertArraysEqual = require('./assertArraysEqual');
 const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const findKeyByValue = require('./findKeyByValue');
+const flatten = require('./flatten');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
+
 
 module.exports = {
   head: head,
@@ -10,4 +23,17 @@ module.exports = {
   tail: tail,
   assertArraysEqual: assertArraysEqual,
   assertEqual: assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countLetters: countLetters,
+  countOnly: countOnly,
+  eqArrays: eqArrays,
+  eqObjects: eqObjects,
+  findKey: findKey,
+  findKeyByValue: findKeyByValue,
+  flatten: flatten,
+  letterPositions: letterPositions,
+  map: map,
+  takeUntil: takeUntil,
+  without: without
+
 };
